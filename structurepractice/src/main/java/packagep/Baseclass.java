ee7b89a11209a16e6a3816d81f36d3e7a385d7c3
Atchaya <atchaya29sumathi@gmail.com>
2025-07-23 15:09:43 +0530

    commiting

---
+++ structurepractice/src/main/java/packagep/Baseclass.java
@@ -0,0 +1,27 @@
+package packagep;
+
+public class Baseclass {
+
+	int x=10;
+	
+	public void Baseclass()
+	{  
+		String test1="abc";
+		System.out.println("printing baseclass");
+	}
+	
+	public  void test() {
+		String test1="bbc";
+		
+		
+	}
+	
+	System.out.println(super.test1);
+	
+	
+	
+}
+		
+		
+
+
---
+++ structurepractice/src/main/java/packagep/testcase1.java
@@ -0,0 +1,15 @@
+package packagep;
+
+public class testcase1 extends Baseclass {
+	
+	
+	public testcase1() {
+		super();
+		
+	}
+		
+	public static void main(String args[]) {
+		
+		Baseclass.test();
+	}
+}
